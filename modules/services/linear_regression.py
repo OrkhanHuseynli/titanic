f9f4ca1426e2e5bda40d8b9943f5c4e2eaa4dc39
OrkhanHuseynli <o.huseynli@gmail.com>
2019-09-22 13:15:46 +0200

    linear regression test

--- modules/services/linear_regression.py
+++ modules/services/linear_regression.py
@@ -8,17 +8,17 @@ from modules.services.dataset_processor import DatasetProcessor
 
 
 class LinearRegression:
-    def __init__(self, dataset, array_of_X_col, y_column_index):
-        self.dataset = dataset
+    def __init__(self, proccessed_dataset, array_of_X_col, y_column_index):
+        self.processed_dataset = proccessed_dataset
         self.array_of_X_col = array_of_X_col
         self.y_column_index = y_column_index
 
     #Gradient descent
     def gradient_descent(self, alpha, iterations):
-        data_processor = DatasetProcessor(self.dataset)
+        data_processor = DatasetProcessor(self.processed_dataset)
         data_processor.normalize()
         X, y, theta = data_processor.create_matricies_and_theta(self.array_of_X_col, self.y_column_index)
-        theta, cost = self.__gradient_descent__(X, y, theta, alpha, iterations)
+        return self.__gradient_descent__(X, y, theta, alpha, iterations)
 
 
     #Gradient descent
@@ -26,7 +26,7 @@ class LinearRegression:
         cost = np.zeros(iterations)
         for i in range(iterations):
             theta = theta - (alpha / len(X)) * np.sum(X * (X @ theta.T - y), axis=0)
-            cost[i] = self.computeCost(X, y, theta)
+            cost[i] = self.compute_cost(X, y, theta)
         return theta, cost
 
     #Cost function
@@ -34,10 +34,10 @@ class LinearRegression:
         for_sum = np.power(((X @ theta.T) - y), 2)
         return np.sum(for_sum) / (2 * len(X))
 
-    def transform_indicies_to_colnames(self, indices):
-        for i in range(len(indices)):
-            indices[i] = self.dataset.columns[indices[i]]
-        return indices
+    # def transform_indicies_to_colnames(self, indices):
+    #     for i in range(len(indices)):
+    #         indices[i] = self.dataset.columns[indices[i]]
+    #     return indices
 
     # @staticmethod
     # def run_regression(file_name):
--- tests/services/test_linear_regression.py
+++ tests/services/test_linear_regression.py
@@ -1,17 +1,42 @@
 from unittest import TestCase
 
+import numpy as np
 import pandas as pd
 
 from modules.core.utils import Utils
+from modules.services.dataset_processor import DatasetProcessor
 from modules.services.linear_regression import LinearRegression
 
 
 class TestLinearRegression(TestCase):
-    print("")
-    # def test_linear_regression(self):
-    #     file_name = "titanic.csv"
-    #     test_file_path = '../resources/' + file_name
-    #     with open(test_file_path, 'rb') as test_file:
-    #         hashed_file_name = Utils.get_hashed_name(file_name) + ".csv"
-    #         Utils.write_file(hashed_file_name, test_file.read(), Utils.UPLOADS_FOLDER)
-    #         LinearRegression.run_regression(file_name)
+    def test_compute_cost(self):
+        alpha = 0.01
+        array_of_X_col, y_column_index = [0, 1], 2
+        data_processor, lin_reg = self.__get_data_processor_and_lin_reg__(array_of_X_col, y_column_index)
+        X, y, theta = data_processor.create_matricies_and_theta(array_of_X_col, y_column_index)
+        theta = theta - (alpha / len(X)) * np.sum(X * (X @ theta.T - y), axis=0)
+        cost = lin_reg.compute_cost(X, y, theta)
+        expected_cost = 0.48054910410767177
+        self.assertEqual(expected_cost, cost)
+
+    def test_gradient_descent(self):
+        alpha = 0.01
+        iterations = 1000
+        array_of_X_col, y_column_index = [0, 1], 2
+        data_processor, lin_reg = self.__get_data_processor_and_lin_reg__(array_of_X_col, y_column_index)
+        gd, cost = lin_reg.gradient_descent(alpha, iterations)
+        print(gd)
+        X_, y_, theta_ = data_processor.create_matricies_and_theta([0, 1], 2)
+        final_cost = lin_reg.compute_cost(X_, y_, gd)
+        print(final_cost)
+        expected_final_cost = 0.13070336960771892
+        self.assertEqual(expected_final_cost, final_cost)
+
+    def __get_data_processor_and_lin_reg__(self, array_of_X_col, y_column_index):
+        file_name = "test_data.csv"
+        test_file_path = '../resources/' + file_name
+        test_dataset = pd.read_csv(test_file_path)
+        data_processor = DatasetProcessor(test_dataset)
+        data_processor.normalize()
+        lin_reg = LinearRegression(data_processor.get_dataset(), array_of_X_col, y_column_index)
+        return data_processor, lin_reg
